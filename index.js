1194fb33489ba60ecb80494e00bdef8b11196d2e
nicorubioritchie <tecladepiano@gmail.com>
2023-05-11 10:12:21 +0200

    principalCambio

---
+++ index.js
@@ -0,0 +1,9 @@
+var nombreAlumno = "Nicolas Rubio";
+var curso = "Primero DAM";
+var temaProyecto = "Página web sobre vinos";
+
+var mensaje = "Nombre del alumno: " + nombreAlumno + "\n" +
+              "Curso: " + curso + "\n" +
+              "Tema elegido de Proyecto Final: " + temaProyecto;
+
+alert(mensaje);
