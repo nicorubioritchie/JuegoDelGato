6247ce97ebcd774fd38463cb6591702f0e41e8ce
nicorubioritchie <tecladepiano@gmail.com>
2023-05-28 15:04:51 +0200

    modal

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
-var nombreAlumno = "Nicolas Rubio";
-var curso = "Primero DAM";
-var temaProyecto = "Página web sobre vinos";
-
-var mensaje = "Nombre del alumno: " + nombreAlumno + "\n" +
-              "Curso: " + curso + "\n" +
-              "Tema elegido de Proyecto Final: " + temaProyecto;
-
-alert(mensaje);
 
 function mostrarImagenes() {
     var imageContainer = document.getElementById("imageContainer");
     imageContainer.style.display = "block";
+  }
+
+  function aplicarCambios() {
+    var elemento = document.getElementsByName('colorElegido')[0];
+    console.log('color elegido: ' + elemento.value);
+
+    var tam = document.getElementsByName('tamFuente')[0];
+    var tamElegido = tam.options[tam.selectedIndex].value;
+    console.log('tamaño de la fuente: ' + tamElegido);
+
+    var parrafo = document.getElementById('esteParrafo');
+    parrafo.style.fontSize = tamElegido + "px";
+    parrafo.style.color = elemento.value;
+
+    document.documentElement.style.fontSize = tamElegido + "px";
+
+    // Guardar preferencias en el almacenamiento local
+    localStorage.setItem('colorElegido', elemento.value);
+    localStorage.setItem('tamFuenteElegido', tamElegido);
+
+    // Aplicar preferencias en todas las ventanas abiertas
+    var ventanas = window.parent.frames;
+    for (var i = 0; i < ventanas.length; i++) {
+        var ventana = ventanas[i];
+        ventana.document.getElementById('esteParrafo').style.fontSize = tamElegido + "px";
+        ventana.document.getElementById('esteParrafo').style.color = elemento.value;
+        ventana.document.documentElement.style.fontSize = tamElegido + "px";
+    }
+}
+
+// Cargar preferencias almacenadas al cargar la página
+window.onload = function() {
+    var colorElegido = localStorage.getItem('colorElegido');
+    var tamFuenteElegido = localStorage.getItem('tamFuenteElegido');
+
+    if (colorElegido && tamFuenteElegido) {
+        document.getElementsByName('colorElegido')[0].value = colorElegido;
+        document.getElementsByName('tamFuente')[0].value = tamFuenteElegido;
+    }
   }
